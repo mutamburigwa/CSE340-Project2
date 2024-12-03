12133ff08607edaa76a8faadec6071b70eeb2cd0
Takudzwa Mutamburigwa <mut23001@byui.edu>
2024-12-03 15:35:49 +0200

    index route update

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,17 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');  // Ensure passport is imported
+const router = require("express").Router();
 
-router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => {
-    //#swagger.tags=['Welcome']
-    res.send('Welcome to Point of Sale'); // Updated welcome message
-});
-
+router.use("/", require("./swagger"));
 router.use('/products', require('./products'));
 router.use('/transactions', require('./transactions'));
 
-// Route for logging in via GitHub
-router.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
+router.get("/login", passport.authenticate("github"), (req, res) => {
+    res.redirect("/");
+});
 
-// Callback route after GitHub authentication
-router.get('/auth/github/callback', 
-    passport.authenticate('github', { failureRedirect: '/' }),
-    (req, res) => {
-        // On successful authentication, redirect to the dashboard or another protected page
-        res.redirect('/dashboard');  // Change this as needed
-    }
-);
-
-// Logout route
-router.get('/logout', function(req, res, next) {
+router.get("/logout", function(req, res, next) {
     req.logout(function(err) {
         if (err) return next(err);
-        res.redirect('/');  // Redirect to the homepage after logout
+        res.redirect("/");
     });
 });
 
